163e1b61da6b55b67620b91f886c5d7108fd303e
Patrick Murray <g00344530@gmit.ie>
2022-01-20 15:50:42 +0000

    Added input box to add patient name and type of data to capture

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,21 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject patientDetails;
 
     public void FollowDot()
     {
         SceneManager.LoadScene("DotFollow");
+    }
+
+    public void showPatientDetails()
+    {
+        patientDetails.SetActive(true);
     }
 }
