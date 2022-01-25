2effcfb5a4b5a96e4e4b635b93187000cf4c0833
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-01-25 00:16:30 +0000

    detected x and y coordinates, scattered a plot of pupil movement

--- eyeMotion.py
+++ eyeMotion.py
@@ -38,7 +38,7 @@ def detect_eyes(img, cascade):
     left_eye = None
     right_eye = None
     for (x, y, w, h) in eyes:
-        if y > height / 2:
+        if y > height / 2: # skip if eye is at the bottom
             pass
         eyecenter = x + w / 2  # get the eye center
         if eyecenter < width * 0.5:
@@ -77,6 +77,17 @@ def blob_process(img, threshold, detector):
 def nothing(x):
     pass
 
+
+# Custom draw keyPoints method
+def draw_custom_KeyPoints(img,keypoints, col, thickness):
+    for current_kp in keypoints:
+        x=np.int(current_kp.pt[0])
+        y=np.int(current_kp.pt[1])
+        size = np.int(current_kp.size/2) # used as a radius of a circle
+        cv2.circle(img,(x,y),size,col,thickness=thickness, lineType=8, shift=0)
+    #plt.imshow(img)
+    return img
+
 # Coordinates of an eye
 x = []
 y = []
@@ -85,25 +96,33 @@ def main():
     cap = cv2.VideoCapture(0)
     cv2.namedWindow('image')
     cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
-
+    i = 0
     while True:
         _, frame = cap.read()
         face_frame = detect_faces(frame, face_cascade)
         if face_frame is not None:
             eyes = detect_eyes(face_frame, eye_cascade)
-            for eye in eyes:               
+            for eye in eyes:     
+                #if i == 2:
+                    #i = 0 
                 if eye is not None:
+                    #print("eye " + (i+1))
                     threshold = r = cv2.getTrackbarPos('threshold', 'image')
                     eye = cut_eyebrows(eye)
+                    #print('an eyes {eyes}'.format(eyes=eyes))
                     keypoints = blob_process(eye, threshold, detector)
-                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                    #eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                    eye = draw_custom_KeyPoints(eye,keypoints,(0,255,0),1)
                     kp = cv2.KeyPoint_convert(keypoints) # converts keypoints to ndarray 
                     if keypoints: #if the keypoint is null it skips it
                         x.append(kp.flat[0]) #adds the x coords
                         y.append(kp.flat[1]) #1d iterator over the array
+                        #x.append(keypoints.pt[0])
+                        #y.append(keypoints.pt[1])
                         print('x: {x}, y: {y}'.format( x=kp.flat[0], y=kp.flat[1]))
                         # Drawing the points where an eye is currently look at
-                        cv2.circle(eye,(int(kp.flat[0]), int(kp.flat[1])), 2,1)
+                       
+                            #cv2.circle(eye,(int(kp.flat[0]), int(kp.flat[1])), 2,1)
         
         cv2.imshow('image', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -111,7 +130,8 @@ def main():
     cap.release()
     #cv2.destroyAllWindows()
     i = 0
-    plt.scatter(x, y) #plot the points
+    plt.imshow(eye,zorder=1)
+    plt.scatter(x, y,zorder =2) #plot the points
     plt.show()
 
 
