01bfdf7638d1b3bc55034c8023ef2972721bca0e
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-01-25 22:23:44 +0000

    merged branches

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from flask import Flask, jsonify
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,10 +11,22 @@ def index():
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 3, 7, 5]
 
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, img = cap.read()
+
+        cv2.imshow('img', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
     #plt.scatter(x, y)
     #plt.show()
     print("Running Python")
-    return "Okay"
+    return "OK"
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
