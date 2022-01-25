32f054018d2cbb770b8b532726dd807dced9846f
Paddy Murray <g00344530@gmit.ie>
2022-01-25 21:36:26 +0000

    Minor changes to ui

--- Assets/Scripts/StartPython.cs
+++ Assets/Scripts/StartPython.cs
@@ -9,7 +9,7 @@ public class StartPython : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(GetRequest("http://127.0.0.1:5000/"));
+        StartCoroutine(GetRequest("http://127.0.0.1:5000/"));
     }
 
     IEnumerator GetRequest(string uri)
--- app.py
+++ app.py
@@ -9,8 +9,10 @@ def index():
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 3, 7, 5]
 
-    plt.scatter(x, y)
-    plt.show()
+    #plt.scatter(x, y)
+    #plt.show()
+    print("Running Python")
+    return "Okay"
 
 if __name__ == "__main__":
     app.run()
