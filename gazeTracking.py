e360c55bf7422814514bb051c1cc71255c1f05c2
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-02-22 17:33:50 +0000

    added pupils location method

--- gazeTracking.py
+++ gazeTracking.py
@@ -22,3 +22,32 @@ class GazeTracking(object):
         self._detector = dlib.get_frontal_face_detector()
         # _predictor is used to get facial landmarks of a given face
         self._predictor = dlib.shape_predictor("shape_68.dat")
+
+    
+    @property
+    def pupils_located(self):
+        """Check that the pupils have been located"""
+        try:
+            int(self.eye_left.pupil.x)
+            int(self.eye_left.pupil.y)
+            int(self.eye_right.pupil.x)
+            int(self.eye_right.pupil.y)
+            return True
+        except Exception:
+            return False
+    
+    def _analyze(self):
+        """Detects the face and initialize Eye objects"""
+
+        # change the frame to grayscale for quicker computations
+        frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        faces = self._face_detector(frame)
+
+        try:
+            landmarks = self._predictor(frame, faces[0])
+            self.eye_left = Eye(frame, landmarks, 0, self.calibration)
+            self.eye_right = Eye(frame, landmarks, 1, self.calibration)
+
+        except IndexError:
+            self.eye_left = None
+            self.eye_right = None
