8cbaf3419258dd7ee776b7e63662125dd9032360
Paddy Murray <g00344530@gmit.ie>
2022-02-22 17:03:43 +0000

    Process the eye frame and isolate the iris

--- calibration.py
+++ calibration.py
@@ -12,3 +12,13 @@ class Pupil(object):
         self.threshold = threshold
         self.x = None
         self.y = None
+    
+    @staticmethod
+    def image_processing(eye_frame, threshold):
+        # Isolates the iris
+        kernel = np.ones((3, 3), np.uint8)
+        new_frame = cv2.bilateralFilter(eye_frame, 10, 15, 15)
+        new_frame = cv2.erode(new_frame, kernel, iterations=3)
+        new_frame = cv2.threshold(new_frame, threshold, 255, cv2.THRESH_BINARY)[1]
+
+        return new_frame
