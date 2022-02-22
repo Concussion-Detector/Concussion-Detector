0755475fb4fcd84a18cf891da99d9571a158ab13
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-02-22 17:15:20 +0000

    Updated threshold method in calibration class

--- calibration.py
+++ calibration.py
@@ -12,3 +12,27 @@ class Calibration(object):
         self.nb_frames = 20
         self.thresholds_left = []
         self.thresholds_right = []
+
+    def is_complete(self):
+        """Checks if calibration is completed"""
+        return len(self.thresholds_left) >= self.nb_frames and len(self.thresholds_right) >= self.nb_frames
+
+    def threshold(self, side):
+        """Returns the threshold value for the given eye."""
+        
+        if side == 0:
+            return int(sum(self.thresholds_left) / len(self.thresholds_left))
+        elif side == 1:
+            return int(sum(self.thresholds_right) / len(self.thresholds_right))
+
+    @staticmethod
+    def iris_size(frame):
+        """Returns the percentage of space that the iris takes up on
+        the surface of the eye.
+        """
+        frame = frame[5:-5, 5:-5]
+        height, width = frame.shape[:2]
+        nb_pixels = height * width
+        nb_blacks = nb_pixels - cv2.countNonZero(frame)
+        return nb_blacks / nb_pixels
+    
