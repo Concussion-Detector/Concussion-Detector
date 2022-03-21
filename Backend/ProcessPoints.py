59a857f4b63c3f82489804fef3307923b3ec8406
Paddy Murray <g00344530@gmit.ie>
2022-03-21 14:47:05 +0000

    Get midpoints for x and y

--- Backend/ProcessPoints.py
+++ Backend/ProcessPoints.py
@@ -4,7 +4,6 @@ class ProcessPoints():
 
     def __init__(self):
         self.ReadCSV()
-
         self.AnalysePoints()
     
     def ReadCSV(self):
@@ -14,14 +13,26 @@ class ProcessPoints():
         with open('./files/eye-coordinatesCSV.csv', 'r') as f:
             reader = csv.reader(f, delimiter = ',')
             for row in reader:
-                x.append(row[0])
-                y.append(row[1])
+                x.append(int(row[0]))
+                y.append(int(row[1]))
             self.x = x
             self.y = y
 
     def AnalysePoints(self):
-        print('x')
-        print(self.x)
-        print('y')
-        print(self.y)
+        minX = min(self.x)
+        minY = min(self.y)
+        maxX = max(self.x)
+        maxY = max(self.y)
+
+        print(minX)
+        print(minY)
+        print(maxX)
+        print(maxY)
+
+        midX = (maxX + minX) / 2
+        midY = (maxY + minY) / 2
+
+        print("The midpoint of {maxx} & {minx} = {midx}".format(maxx=maxX, minx=minX,midx=midX))
+        #print(midX)
+        #print(midY)
 
