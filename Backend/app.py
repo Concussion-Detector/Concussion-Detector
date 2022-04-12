fc2161c2ee92ebd32d9c7242344057cf56103db2
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-04-12 22:54:36 +0100

    Prepare for heroku deploy

--- Backend/app.py
+++ Backend/app.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template, Response
+from sys import stdout
+from process import webopencv
 import cv2
+import logging
+from flask import Flask, render_template, Response, request, jsonify
+from flask_socketio import SocketIO
+from camera import Camera
 from eye_tracking import GazeTracking
+from utils import base64_to_pil_image, pil_image_to_base64
+import os
 
-cap = cv2.VideoCapture(0)
 gaze = GazeTracking()
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(webopencv())
+
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    #camera.enqueue_input(base64_to_pil_image(input))
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    app.logger.info("client connected")
 
 xpupil = []
 ypupil = []
 
-@app.route('/')
-
-
 # save coords in csv file
-
+@app.route('/')
 def index():
     return  render_template('index.html')
 
 def face_detection():
     while True:
-        _, frame = cap.read()
+        _, frame = camera.get_frame()
         # Analyze frame
         gaze.refresh(frame)
 
@@ -32,15 +50,15 @@ def face_detection():
         cv2.putText(frame, "Left pupil:  " + str(left_pupil), (20, 70), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
         cv2.putText(frame, "Right pupil: " + str(right_pupil), (20, 105), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-        ret, buffer = cv2.imencode('.jpg',frame)
-        frame = buffer.tobytes()
+        # ret, buffer = cv2.imencode('.jpg',frame)
+        # frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n'+frame+ b'\r\n')
         
         
 @app.route('/track')
-
 def track():
     return Response(face_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(debug=True)
+#app.run(debug=True)
+socketio.run(app)
