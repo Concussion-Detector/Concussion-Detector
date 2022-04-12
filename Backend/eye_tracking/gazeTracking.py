dc4fafaec8a57ee81d4eabc87c7d5543e9b11b83
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2022-04-12 22:32:55 +0100

    Refactored gaze tracking

--- Backend/eye_tracking/gazeTracking.py
+++ Backend/eye_tracking/gazeTracking.py
@@ -42,7 +42,7 @@ class GazeTracking(object):
     def _analyze(self):
         """Detects the face and initialize Eye objects"""
 
-        # change the frame to grayscale for quicker computations
+        # Change the frame to grayscale for quicker computations
         frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         faces = self._detector(frame)
 
@@ -75,26 +75,6 @@ class GazeTracking(object):
             y = self.eye_right.origin[1] + self.eye_right.pupil.y
             return (x, y)
     
-    def horizontal_ratio(self):
-        """Returns a number between 0.0 and 1.0 that indicates the
-        horizontal direction of the gaze. The extreme right is 0.0,
-        the center is 0.5 and the extreme left is 1.0
-        """
-        if self.pupils_located:
-            pupil_left = self.eye_left.pupil.x / (self.eye_left.center[0] * 2 - 10)
-            pupil_right = self.eye_right.pupil.x / (self.eye_right.center[0] * 2 - 10)
-            return (pupil_left + pupil_right) / 2
-
-    def vertical_ratio(self):
-        """Returns a number between 0.0 and 1.0 that indicates the
-        vertical direction of the gaze. The extreme top is 0.0,
-        the center is 0.5 and the extreme bottom is 1.0
-        """
-        if self.pupils_located:
-            pupil_left = self.eye_left.pupil.y / (self.eye_left.center[1] * 2 - 10)
-            pupil_right = self.eye_right.pupil.y / (self.eye_right.center[1] * 2 - 10)
-            return (pupil_left + pupil_right) / 2
-
     def draw(self):
         """Returns the main frame draws coords of the pupils and 
         rectangle on the detected face to help with proper face alignment."""
